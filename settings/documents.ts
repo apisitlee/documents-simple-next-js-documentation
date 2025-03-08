efd3284e7f071e70b18692fb818d32ae17cf3ca5
Apisit Lee <138636823+apisitlee@users.noreply.github.com>
2025-03-08 18:07:31 +0800

    upd4

--- settings/documents.ts
+++ settings/documents.ts
@@ -8,6 +8,22 @@ export const Documents: Paths[] = [
       {
         title: "儒家",
         href: "/ru-jia",
+        items: [
+          {
+            title: "论语",
+            href: "/lun-yu",
+            items: [
+              {
+                title: "学而",
+                href: "/xue-er"
+              },
+              {
+                title: "为政",
+                href: "/wei-zheng"
+              }
+            ]
+          }
+        ]
       }
     ],
   },
